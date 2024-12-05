901b8795e25029c2b8657d10212980b855db85dc
Melissa Perdomo <melissameira92@gmail.com>
2024-12-05 17:20:18 -0300

    Últimos ajustes

--- js/index.js
+++ js/index.js
@@ -8,16 +8,13 @@ const yellowDashElements = document.querySelectorAll('.yellow-dash');
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // Verifica se a animação já foi ativada
-            if (!entry.target.classList.contains('animate')) {
-                entry.target.classList.add('animate');
-                observer.unobserve(entry.target); // Remove do Observer após animar
-            }
+            // Adiciona a classe 'animate' quando o elemento entra na viewport
+            entry.target.classList.add('animate');
+            observer.unobserve(entry.target); // Para de observar após animar
         }
     });
 }, {
-    threshold: 0.5 // 50% do elemento visível
+    threshold: 0.5 // Ativa quando 50% do elemento está visível
 });
 
-// Observa cada elemento
 yellowDashElements.forEach(element => observer.observe(element));
