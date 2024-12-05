c17a9fe5f645c5458f40c953ee88257a431d448d
Melissa Perdomo <melissameira92@gmail.com>
2024-12-05 08:54:02 -0300

    Começando as animações

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@ function openMenu() {
     let menu = document.querySelector(".header-right");
     menu.classList.toggle("hide");
 }
+
+const yellowDashElements = document.querySelectorAll('.yellow-dash');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Verifica se a animação já foi ativada
+            if (!entry.target.classList.contains('animate')) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target); // Remove do Observer após animar
+            }
+        }
+    });
+}, {
+    threshold: 0.5 // 50% do elemento visível
+});
+
+// Observa cada elemento
+yellowDashElements.forEach(element => observer.observe(element));
