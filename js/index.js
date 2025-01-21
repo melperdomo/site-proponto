e328f583f16e31616de62b0705941c348d2d459d
Mel Perdomo <melissa.perdomo@proponto.com.br>
2025-01-21 15:50:17 -0300

    Ajustes Felps.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@ function openMenu() {
     let menu = document.querySelector(".header-right");
     menu.classList.toggle("hide");
 }
+
+function openCase() {
+    const shadow_bg = document.querySelector(".shadow-bg");
+    shadow_bg.classList.remove('hidden');
+}
+
+function closeCase() {
+    const shadow_bg = document.querySelector(".shadow-bg");
+    shadow_bg.classList.add('hidden');
+}
