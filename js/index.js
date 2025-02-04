64463981902f02cd2357cdccff12354bc31c4dbe
Mel Perdomo <melissa.perdomo@proponto.com.br>
2025-02-04 14:54:46 -0300

    Ajustes mobile

--- js/index.js
+++ js/index.js
@@ -3,12 +3,17 @@ function openMenu() {
     menu.classList.toggle("hide");
 }
 
-function openCase() {
+function openCase(flex_case) {
+    const f_case = document.querySelector(flex_case);
     const shadow_bg = document.querySelector(".shadow-bg");
     shadow_bg.classList.remove('hidden');
+    f_case.classList.remove('none');
 }
 
 function closeCase() {
     const shadow_bg = document.querySelector(".shadow-bg");
     shadow_bg.classList.add('hidden');
+    for (let f_slider of shadow_bg.querySelectorAll('flex-slider')) {
+        f_slider.classList.add('none');
+    }
 }
